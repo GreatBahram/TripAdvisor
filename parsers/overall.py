947d49a1f5e742811f50d0757a38afe83b715d2b
GreatBahram <aghaee.bahram@gmail.com>
2018-09-12 17:47:51 +0430

    improve overall parser

--- parsers/overall.py
+++ parsers/overall.py
@@ -6,16 +6,20 @@ def overall_review_numbers(html_codes, uri, city):
     """ """
     soup = BeautifulSoup(html_codes, 'html.parser')
     overall_dict = {
-            'hotels': 'hotels',
-            'forum': 'forum',
-            'restaurants': 'restaurants',
-            'flights': 'flights', 
-            'thingstodo': 'attractions',
-            'vacationalrentals': 'vacationRentals',
+            'num_of_vactionalrentals': '.vacationrentals  .typeQty',
+            'vacationalrentals': '.vacationrentals .contentCount',
+            'num_of_restaurants': '.restaurants .typeQty',
+            'restaurants': '.restaurants .contentCount',
+            'num_of_hotels': '.hotels .typeQty',
+            'hotels': '.hotels .contentCount',
+            'num_of_thingstodo': '.attractions  .typeQty',
+            'thingstodo': '.attractions .contentCount',
+            'forum': '.forum .contentCount',
+            'flights': '.flights .contentCount', 
             }
     for key in overall_dict:
         try:
-            overall_dict[key] = soup.select('.{} .contentCount'.format(overall_dict[key]))[0].getText().split()[0]
+            overall_dict[key] = soup.select('{}'.format(overall_dict[key]))[0].getText().split()[0]
         except IndexError:
             overall_dict[key] = 0
 
