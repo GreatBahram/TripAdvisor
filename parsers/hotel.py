82d0cc461112017ac7c6a3a13af9be9ecf5a0206
GreatBahram <aghaee.bahram@gmail.com>
2018-09-26 18:16:42 +0330

    add changes into hotel module

--- parsers/hotel.py
+++ parsers/hotel.py
@@ -1,27 +1,15 @@
+# Third party imports
 import requests
-
+from bs4 import BeautifulSoup
 from lxml import html
 from lxml.etree import tostring
 
 
 class HotelParser:
-    def set_database(self,database):
-        self.database = database
-
-    def set_city_name(self,city_name):
-        self.city_name = city_name
-
-    def set_hotel(self, hotel_link):
-        self.trip_advisor = 'https://www.tripadvisor.com'
+    def __init__(self, hotel_link):
         self.hotel_link = hotel_link
-        self.name = self.get_hotel_name()
-        if self.name is None:
-            return None
-        self.Session = requests.session()
-        self.rw_count = 0;
-
-    def get_review_count(self):
-        return self.rw_count
+        self.name = self.get_name()
+        self.session = requests.session()
 
     def find_variable(self, text, variable):
         a = text.find(variable) + len(variable)
@@ -30,96 +18,73 @@ class HotelParser:
 
     def open_hotel_page(self, hotel_link):
         try:
-            response_hotel_page = self.Session.get(hotel_link)
-            tree = html.fromstring(response_hotel_page.text)
-            review_ids = list(set(tree.xpath(
-                '//*[contains(concat( " ", @class, " " ), concat( " ", "review-container", " " ))]/@data-reviewid')))
+            response_hotel_page = self.session.get(hotel_link).text
+            soup = BeautifulSoup(response_hotel_page, 'lxml')
+            review_ids = [review.attrs['data-reviewid'] for review in soup.select('.review-container')]
             data_val = {'reviews': ','.join(review_ids)}
             headers = {'Referer': hotel_link}
-            return self.Session.post(
+            return self.session.post(
                 'https://www.tripadvisor.ca/OverlayWidgetAjax?Mode=EXPANDED_HOTEL_REVIEWS_RESP&metaReferer=',
                 data=data_val,
-                headers=headers)
+                headers=headers), response_hotel_page
+        except Exception as e:
+            print(e)
+            return None, None
+
+    def get_name(self):
+        try:
+            return self.hotel_link.partition('-Reviews-')[2].split('-')[0]
         except Exception as e:
             return None
 
-    def get_hotel_name(self):
-        try:
-            return self.hotel_link[self.hotel_link.find('-Reviews-') + 9: self.hotel_link.find('-',
-                                                                                               self.hotel_link.find(
-                                                                                                   '-Reviews-') + 9)]
-        except Exception as e:
-            return None
+    def _return_rating_code(self, class_):
+        rating_code = {
+                'bubble_50': '5.0',
+                'bubble_45': '4.5',
+                'bubble_40': '4.0',
+                'bubble_35': '3.5',
+                'bubble_30': '3.0',
+                'bubble_25': '2.5',
+                'bubble_20': '2.0',
+                'bubble_15': '1.5',
+                'bubble_10': '1.0',
+                'bubble_05': '0.5',
+                }
+        return rating_code.get(class_, 'N/A')
 
     def get_hotel_views_in_this_page(self, page):
         output_list = []
-        tree = html.fromstring(page.text)
-        reviews = list(set(tree.xpath('//div[@class="reviewSelector"]')))
-        for _ in reviews:
-            try:
-                data = {}
-                tree = html.fromstring(str(tostring(_)))
-                review_list = list(tree.xpath('//div[@class="entry"]'))
-                review_text = ''
-                tree_ = html.fromstring(str(tostring(review_list[0])))
-                review_text_list = list(tree_.xpath('//p[@class="partial_entry"]/text()'))
-                for text in review_text_list:
-                    review_text = review_text + text
-                user_id = list(set(tree.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "avatar", " " ))]/@class')))[0]
-                user_id = user_id[15:]
-                title = list(set(tree.xpath('//span[contains(concat( " ", @class, " " ), concat( " ", "noQuotes", " " ))]/text()')))[0]
-                rating_date = list(set(tree.xpath('//span[contains(concat( " ", @class, " " ), concat( " ", "ratingDate", " " ))]/@title')))[0]
-                stay_date = ''
-                try:
-                    stay_date = list(set(tree.xpath('//div[contains(concat( " ", @class, " " ), concat( " ", "recommend-titleInline", " " ))]/text()')))[0]
-                except:
-                    stay_date = ''
-                rate_val = -1
-                if len(list(set(tree.xpath('//span[contains(concat( " ", @class, " " ), concat( " ", "ui_bubble_rating bubble_50", " " ))]')))) :
-                    rate_val = 5
-                elif len(list(set(tree.xpath('//span[contains(concat( " ", @class, " " ), concat( " ", "ui_bubble_rating bubble_40", " " ))]')))) :
-                    rate_val = 4
-                elif len(list(set(tree.xpath('//span[contains(concat( " ", @class, " " ), concat( " ", "ui_bubble_rating bubble_30", " " ))]')))) :
-                    rate_val = 3
-                elif len(list(set(tree.xpath('//span[contains(concat( " ", @class, " " ), concat( " ", "ui_bubble_rating bubble_20", " " ))]')))) :
-                    rate_val = 2
-                elif len(list(set(tree.xpath('//span[contains(concat( " ", @class, " " ), concat( " ", "ui_bubble_rating bubble_10", " " ))]')))) :
-                    rate_val = 1
-                self.database.add_reveiw(review_text, title, rating_date,rate_val,'', user_id, self.__class__.__name__, self.name, self.city_name)
-                self.rw_count += 1
-
-            #     data['review_text'] = review_text
-            #     data['title'] = title
-            #     data['user_id'] = user_id
-            #     data['rating_date'] = rating_date
-            #     data['stay_date'] = stay_date
-            #     output_list.append(data)
-            # return output_list
-            except Exception as e:
-                continue
+        soup = BeautifulSoup(page.text, 'lxml')
+        reviews = soup.select('.reviewSelector')
+        for review in reviews:
+            data = {}
+            data['review_text'] = review.select_one('.partial_entry').getText()
+            data['rate'] = self._return_rating_code(review.select_one('.ui_bubble_rating').attrs['class'][1])
+            data['user_id'] = review.select_one('.avatar').attrs['class'][-1]
+            data['title'] = review.select_one('.noQuotes').getText()
+            data['review-date'] = review.select_one('.ratingDate').attrs['title']
+            data['stayed-date'] = review.select_one('.recommend-titleInline').getText().partition('Stayed: ')[-1].split(',')[0]
+            output_list.append(data)
+        return output_list
 
     def get_all_hotel_reviews(self):
-        response_hotel_page = self.Session.get(self.hotel_link)
-        hotel_link = self.hotel_link
-        tree = html.fromstring(response_hotel_page.text)
+        post_data, get_data = self.open_hotel_page(self.hotel_link)
+        list_views = self.get_hotel_views_in_this_page(post_data)
         try:
-            page_numbers = list(set(tree.xpath(
-                '//div[@id="REVIEWS"]//div[contains(concat( " ", @class, " " ), concat( " ", "ui_pagination", '
-                '" " ))]/div/a/@data-page-number')))
-            page_numbers = max(list(map(int, page_numbers)))
+            soup2 = BeautifulSoup(get_data, 'lxml')
+            first_page_number = int(soup2.select_one('a.pageNum.first.current').attrs['data-page-number'])
+            last_page_number = int(soup2.select_one('a.pageNum.last.taLnk').attrs['data-page-number'])
         except:
-            page_numbers = -1
+            first_page_number = last_page_number = None
 
-        hotel_page_content = self.open_hotel_page(self.hotel_link)
-        self.get_hotel_views_in_this_page(hotel_page_content)
-        # list_views = self.get_hotel_views_in_this_page(hotel_page_content)
+        if first_page_number:
+            for page_number in range(first_page_number, last_page_number):
+                next_page_link = self.hotel_link.partition('-Reviews-')[0] + '-or' + str(page_number * 5) + '-' + self.hotel_link.partition('-Reviews-')[2]
+                print(next_page_link)
+                #post_data, _ = self.open_restaurant_page(next_page_link)
+                #list_views += self.get_restaurant_views_in_this_page(post_data)
+        #return list_views
 
-        if page_numbers != -1:
-            for i in range(1, page_numbers):
-                next_hotel_page = hotel_link[:hotel_link.find('-Reviews-')] \
-                                  + '-or' + str(i * 5) + '-' \
-                                  + hotel_link[hotel_link.find('-Reviews-') + 9:]
-                self.get_hotel_views_in_this_page(self.open_hotel_page(next_hotel_page))
-                # list_views += self.get_hotel_views_in_this_page(self.open_hotel_page(next_hotel_page))
-
-        # return list_views
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
